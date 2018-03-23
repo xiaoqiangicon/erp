9a2865062d6615e1e63c9e7c1ffa5cc1817bbe1e
senntyou <jiangjinbelief@163.com>
2018-03-23 08:31:54 +0800

    complete kind/index/ui

--- static/src/kind/index/ui/index.js
+++ static/src/kind/index/ui/index.js
@@ -3,3 +3,13 @@ require('component/nav');
 require('@zzh/common.css');
 require('less/common.less');
 require('../index.less');
+
+require('@zzh/pagination/dist/pagination.css');
+require('less/pagination.less');
+const Pagination = require('@zzh/pagination');
+
+let pagination = new Pagination('#pagination-container', {
+    totalPages: 20
+});
+
+pagination.render();
