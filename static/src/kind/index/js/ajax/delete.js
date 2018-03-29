f4c99225cb036c671ec5a6ee8e91cfd863a10813
senntyou <jiangjinbelief@163.com>
2018-03-29 10:46:23 +0800

    update

--- static/src/kind/index/js/ajax/delete.js
+++ static/src/kind/index/js/ajax/delete.js
@@ -4,10 +4,18 @@
 
 let seeAjax = require('see-ajax');
 
+let requestKeys = {
+    id: 'charityId'
+};
+
 seeAjax.config('delete', {
     url: [
-        '',
+        '/zzhadmin/charityDel/',
         '/static/src/kind/index/data/delete_server.json',
         '/static/src/kind/index/data/delete.json'
+    ],
+    requestKeys: [
+        requestKeys,
+        requestKeys
     ]
 });
--- static/src/kind/record/js/ajax/main.js
+++ static/src/kind/record/js/ajax/main.js
@@ -3,11 +3,31 @@
  */
 
 let seeAjax = require('see-ajax');
+let zzhUtil = require('@zzh/util');
+
+let responseRefactor = {
+    todayTotalMoney: 'data.todayPrice',
+    todayTotalPeople: 'data.todayJoinNum',
+    totalMoney: 'data.totalPrice',
+    totalPeople: 'data.todayJoinNum'
+};
+
+let preHandle = req => {
+    req.charityId = zzhUtil.urlParams.id;
+};
 
 seeAjax.config('main', {
     url: [
-        '',
+        '/zzhadmin/charityTotalInfo/',
         '/static/src/kind/record/data/main_server.json',
         '/static/src/kind/record/data/main.json'
+    ],
+    responseRefactor: [
+        responseRefactor,
+        responseRefactor
+    ],
+    preHandle: [
+        preHandle,
+        preHandle
     ]
 });
