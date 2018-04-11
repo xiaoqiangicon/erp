6451caf4f5833ce112dae17bc59a7945cb7f85bb
senntyou <jiangjinbelief@163.com>
2018-04-11 14:28:51 +0800

    update

---
+++ static/src/kind/edit/config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    splitJs: {
+        lib: ['jquery', 'handlebars'],
+        ueditor: ['component/ueditor_config', '@zzh/ueditor/src/ueditor.config', '@zzh/ueditor']
+    }
+};
