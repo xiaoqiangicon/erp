2ca39c9ec65437633bce25f6b22768dfaa83a65f
senntyou <jiangjinbelief@163.com>
2018-11-20 17:09:48 +0800

    finish promote/list

---
+++ static/src/promote/list/mock/search.js
@@ -0,0 +1,13 @@
+module.exports = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: [1, 2, 3, 4].map(id => ({
+        id,
+        title: `本焕寺第廿四期施放千台吉祥焰口及供灯法会（农历十月在线登记） ${id}`,
+        added: Math.floor(Math.random() * 2),
+      })),
+    })
+  );
+};
---
+++ static/src/promote/list/tpl.js
@@ -0,0 +1,33 @@
+import handlebars from 'handlebars';
+
+export const rowsTpl = handlebars.compile(`
+{{#each data}}
+<div class="row-1" data-row="{{id}}">
+  <div class="cell-1">{{id}}</div>
+  <div class="cell-1 cell-1-name">
+    <img class="image-1" src="{{cover}}">
+    <span>{{title}}</span>
+  </div>
+  <div class="cell-1">{{count}}</div>
+  <div class="cell-1">{{totalMoney}}</div>
+  <div class="cell-1">{{profit}}</div>
+  <div class="cell-1">
+    <div class="c-switch {{#if online}}active{{/if}}" data-row-status="{{id}}">
+      <i class="circle-1"></i>
+      <span class="text-left-1">上架中</span>
+      <span class="text-right-1">下架中</span>
+    </div>
+  </div>
+  <a class="cell-1 cell-1-handle no-underline" href="./manage.html?id={{id}}">操作</a>
+</div>
+{{/each}}
+`);
+
+export const searchTpl = handlebars.compile(`
+{{#each data}}
+<div class="row-3 {{#if added}}disabled{{/if}}" data-search-row="{{id}}">
+  2018年祈福大典法会2018年祈福大典法会2018年祈福大典法会
+  {{#if added}}<span class="badge-1">已添加</span>{{/if}}
+</div>
+{{/each}}
+`);
