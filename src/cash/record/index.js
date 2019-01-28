565b1087cd43326b59f35409223117cd839bb325
senntyou <jiangjinbelief@163.com>
2019-01-28 15:54:10 +0800

    add cash/record

--- src/cash/record/index.js
+++ src/cash/record/index.js
@@ -5,15 +5,10 @@ import '../../css/common/index.css';
 import 'jquery-confirm/dist/jquery-confirm.min.css';
 import 'colors.css/css/colors.css';
 import '@senntyou/shortcut.css';
-import 'tippy.js/dist/tippy.css';
 import 'toastr/build/toastr.css';
 
-import '@fancyapps/fancybox/dist/jquery.fancybox.css';
-import 'orchids/dist/orchids.css';
-import '@zzh/choose-image/dist/choose-image.css';
 import '../../lib/material-icons.css';
 import '../../lib/bootstrap-material-datetimepicker.css';
-import '../../css/pagination.css';
 
 import './index.css';
 
