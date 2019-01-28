1d201427ec98657e888d04cb1ff1d97f7d7c3c48
senntyou <jiangjinbelief@163.com>
2019-01-28 11:46:02 +0800

    add article/index

---
+++ src/article/index/ui/index.js
@@ -0,0 +1,6 @@
+import '../../../component/nav';
+import '../../../css/common/index.css';
+
+import 'jquery-confirm/dist/jquery-confirm.min.css';
+import 'colors.css/css/colors.css';
+import '../index.css';
