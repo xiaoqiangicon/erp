a07fbf902901fbd9f4d91fc72d229d5917ece224
senntyou <jiangjinbelief@163.com>
2019-01-29 15:18:10 +0800

    add register/*

---
+++ src/register/new_pwd/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../common.css';
+import '../dataurl.css';
+import '../find_pwd/index.css';
+
+import './preset';
+
+import './old';
---
+++ src/register/verify/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../common.css';
+import '../dataurl.css';
+import '../submit_data/index.css';
+import './index.css';
+
+import './preset';
+
+import './old';
