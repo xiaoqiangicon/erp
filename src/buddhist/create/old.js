9438469f3cf083b41abd344e8f9699b8e9466583
kangyong <kangyongxd@163.com>
2019-06-18 10:20:09 +0800

    佛事审核

--- src/buddhist/create/old.js
+++ src/buddhist/create/old.js
@@ -4439,7 +4439,8 @@ define([
             self.change_url();
           } else {
             // 添加审核提醒 需求要求在佛事列表页展示 因此使用 sessionStorage
-            window.sessionStorage.setItem('buddhistVerify', 1);
+            if (!Number(verifyId))
+              window.sessionStorage.setItem('buddhistVerify', 1);
 
             // 非草稿
             if (res.createCalendar !== 1) {
