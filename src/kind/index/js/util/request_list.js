4591422ffa302ef14bf4abea35d34f768a5a5a51
liqiang <liqiang@zizaihome.com>
2019-08-21 15:18:48 +0800

    日行一善剩余次数推送bug

--- src/kind/index/js/util/request_list.js
+++ src/kind/index/js/util/request_list.js
@@ -64,9 +64,9 @@ const requestList = (page, init) => {
             data.list = data.scheduleList.list;
 
             console.log(data.scheduleList);
-            $('#remain-time').text(data.scheduleList.count);
+            $('#remain-time').text(data.scheduleList.todayNum);
             $('.record-content').html(listTpl(data.scheduleList));
-            $('[data-remain-time]').text(data.scheduleList.count);
+            $('[data-remain-time]').text(data.scheduleList.todayNum);
 
             if (!data.scheduleList.list.length) {
               $('[data-no-list]').show();
