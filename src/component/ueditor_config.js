5637048a0f24373922db12d47867eaf2f74fa37e
senntyou <jiangjinbelief@163.com>
2019-10-23 12:19:22 +0800

    remove static/dist

--- src/component/ueditor_config.js
+++ src/component/ueditor_config.js
@@ -5,6 +5,6 @@ window.UEDITOR_HOME_URL =
     : '/static/res/ueditor/';
 window.UEDITOR_SERVER_URL = '/zzhadmin/uploadPicUEditor/';
 window.UEDITOR_MUSIC_IFRAME_URL =
-  '/static/dist/html/component/ueditor_plugins/music_iframe.html';
+  '/static/build/component/ueditor_plugins/music_iframe.html';
 window.UEDITOR_MUSIC_DIALOG_URL =
-  '/static/dist/html/component/ueditor_plugins/music.html';
+  '/static/build/component/ueditor_plugins/music.html';
