2c98c8930fd4144ce5db70817542171559a1a0af
senntyou <jiangjinbelief@163.com>
2020-02-25 09:28:19 +0800

    update kind/record

--- src/kind/record/js/init.js
+++ src/kind/record/js/init.js
@@ -5,4 +5,21 @@ import 'bootstrap-datepicker';
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.zh-CN.min.js';
 import commonTpl from 'common/tpl';
 import mainTpl from './tpl/main';
+import fetchList from './util/request_list';
 import './ajax';
+
+seeAjax('main', {}, res => {
+  $('body').append(mainTpl(res));
+
+  $('#list-container').html(commonTpl.loading);
+
+  fetchList();
+
+  // 初始化日期选择器
+  $('[data-date-input]').datepicker({
+    format: 'yyyy-mm-dd',
+    language: 'zh-CN',
+    autoclose: true,
+    forceParse: !1,
+  });
+});
