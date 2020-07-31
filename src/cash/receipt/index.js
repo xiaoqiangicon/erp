eb7671090c10a75dd2871fd24514cf9085c172d3
liqiang <liqiang@zizaihome.com>
2020-07-31 18:27:58 +0800

    cash/receipt:update

--- src/cash/receipt/index.js
+++ src/cash/receipt/index.js
@@ -4,7 +4,14 @@ import '@senntyou/shortcut.css';
 import './index.less';
 
 import 'element-ui/lib/theme-chalk/index.css';
-import { Button, Table, TableColumn, Pagination, Dialog } from 'element-ui';
+import {
+  Button,
+  Table,
+  TableColumn,
+  Pagination,
+  Dialog,
+  Tooltip,
+} from 'element-ui';
 
 import Vue from 'vue';
 
@@ -19,6 +26,7 @@ Vue.use(Table);
 Vue.use(TableColumn);
 Vue.use(Pagination);
 Vue.use(Dialog);
+Vue.use(Tooltip);
 
 // eslint-disable-next-line no-new
 new Vue({
