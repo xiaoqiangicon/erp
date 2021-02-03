43a51b7894e8c8637a8dd44afe08f67cb9d38422
liqiang <563165732@qq.com>
2021-02-03 12:04:08 +0800

    home/index:修复接口地址未加/的bug

--- src/component/nav/js/api.js
+++ src/component/nav/js/api.js
@@ -2,7 +2,7 @@ import axios from 'util/axios';
 
 export default {
   // 获取寺院推送信息列表
-  getNoticeList: p => axios('get', '/common/getTempleMessageList', p),
+  getNoticeList: p => axios('get', '/common/getTempleMessageList/', p),
   // 更新单条推送信息为已读
-  updateNoticeRead: p => axios('get', '/common/updateTempleMessageRead', p),
+  updateNoticeRead: p => axios('get', '/common/updateTempleMessageRead/', p),
 };
--- src/home/notice/api.js
+++ src/home/notice/api.js
@@ -2,9 +2,9 @@ import axios from 'util/axios';
 
 export default {
   // 获取寺院推送信息列表
-  getNoticeList: p => axios('get', '/common/getTempleMessageList', p),
+  getNoticeList: p => axios('get', '/common/getTempleMessageList/', p),
   // 更新单条推送信息为已读
-  updateNoticeRead: p => axios('get', '/common/updateTempleMessageRead', p),
+  updateNoticeRead: p => axios('get', '/common/updateTempleMessageRead/', p),
   // 获取左侧菜单消息提示数量
   getMenuTipNum: p => axios('get', '/zzhadmin/orderNumGet/', p),
 };
